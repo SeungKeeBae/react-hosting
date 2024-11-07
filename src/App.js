ae4010c29f436b01a6ab81a6047146bfe2d956ce
SeungKeeBae <tmdrl911226@naver.com>
2024-11-07 21:15:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import React, { useCallback, useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider,
+    sendMessage, // unity 함수를 호출하기 위한 sendMessage 추가
+    addEventListener, // unity -> react 통신
+    removeEventListener, // unity -> react 통신
+    UNSAFE__detachAndUnloadImmediate: detachAndUnloadImmediate,
+   } = useUnityContext({
     loaderUrl: "build/react-hosting.loader.js",
     dataUrl: "build/react-hosting.data",
     frameworkUrl: "build/react-hosting.framework.js",
     codeUrl: "build/react-hosting.wasm",
   });
+
+  const RequsetPost = useCallback((url, jsonData) => {
+    Answer(jsonData);
+  }, []);
+
+  useEffect(() => {
+    addEventListener("RequsetPost", RequsetPost);
+
+    return () => {
+      detachAndUnloadImmediate().catch((reason) => {
+        console.log(reason);
+      });
+      removeEventListener("RequsetPost", RequsetPost);
+    };
+  }, [
+    detachAndUnloadImmediate,
+    addEventListener,
+    removeEventListener,
+    setSpeedUp,
+    setSpeedDown,
+  ]);
 
   return (
     <div className="App">
